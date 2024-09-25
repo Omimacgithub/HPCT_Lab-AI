6f8676dafac992ccec48ca076f3d2e95964c68f8
Omimacgithub <90336442+Omimacgithub@users.noreply.github.com>
2024-09-25 20:32:55 +0200

    createnv.sh finished

--- createnv.sh
+++ createnv.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 echo "Load python"
-module load python cesga/system python/3.10.8
+module load python/3.10.8
 echo "Create and activate python venv"
-python3 -m venv ./mypython
+if [ -d "./mypython" ]; then
+  echo "Venv already exist. Skipping step"
+else
+  echo "Creating venv"
+  python3 -m venv ./mypython
+fi
 . ./mypython/bin/activate
 echo "Install requirements"
 pip install -r requirements.txt
 echo "Send the job to execute"
 sbatch job.sh
+wait 1
+echo "To check the job status: watch -n 1 squeue"
