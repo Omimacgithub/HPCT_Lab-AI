35eb12636b8f0d7f3625d5be8aa1bf7c7de22e0b
Omimacgithub <omar.montenegrom@udc.es>
2024-09-25 13:07:13 +0200

    Added profiling information to train

--- README.md
+++ README.md
@@ -7,14 +7,40 @@ Git repository for AI lab
 
 ## SQUAD
 Este dataset nos proporciona un json con los siguientes campos significativos:
+
 - context: proporciona información detallada acerca de un tema (ejemplo: bibliografía de Fréderic Chopin)
 - question: una pregunta acerca de un tema
 - answer: diccionario con los siguientes elementos:
 	- text: respuesta a la pregunta (información para el dataset de test)
 	- answer\_start: offset en nº de caracteres donde comienza la respuesta incluida en el context (información para el dataset de test).
+
 El objetivo es entrenar al modelo BERT para que dado un context **sepa responder correctamente** a las preguntas que se le hacen del tema relacionado con el contexto.
 ##BERT
-A este modelo se le deben pasar las siguientes entradas(features):
+
+Para este modelo son necesarias las siguientes entradas(features):
+
 - input\_ids: en esta entrada se introduce el context y las preguntas asociadas. Cada valor representa un índice que corresponde a la posición de una palabra en un wordlist que usa el modelo(proceso de tokenización de los datos).
 - attention\_mask: indica si el token es una máscara (0) o no (1).
 
+Adicionalmente, añadimos 2 entradas más que el modelo usará para calcular el valor de pérdida (loss):
+
+- start\_position: igual al campo **answer_start** del dataset
+- end\_position: offset en nº de caracteres donde **termina** la respuesta incluida en el context
+
+El modelo computa su respectivo start\_position y end\_position de la que cree que es la respuesta correcta y los compara con los 2 valores que se pasan como entradas (calcula la pérdida).
+
+## How to run it?
+
+**NOTA:** la visualización de la información de profiling en tensorboard **no está soportada para el navegador Safari**.
+
+**En el directorio del repositorio** ejecutamos tensorboard con los datos generados:
+
+~~~shell
+tensorboard --logdir=./runs --bind\_all &
+~~~
+
+Accedemos desde el navegador web a la sección de profiling en tensorboard:
+
+~~~shell
+http://\<IP\_del\_nodo\>:6006/#pytorch\_profiler
+~~~
--- training.py
+++ training.py
@@ -1,45 +1,15 @@
-#import torch
-#import torch.tensor
-from torch.utils.data import Dataset, DataLoader
+import time
+import torch.cuda
+from torch.utils.data import DataLoader
 import torch.profiler
-#import torchvision.datasets
-#import torchvision.transforms as T
 from datasets import load_from_disk
-from datasets import load_dataset
-from transformers import (
-    AutoModelForQuestionAnswering,
-    AutoTokenizer,
-    DefaultDataCollator,
-    Trainer,
-    TrainingArguments,
-)
-
 from tokenize_squad import TOKENIZED_PATH, get_tokenized_datasets
-
-train_loader = None
-
-class SquadDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings):
-        self.encodings = encodings
-
-    def __getitem__(self, idx):
-        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-
-    def __len__(self):
-        return len(self.encodings["input_ids"])
-
+from transformers import TrainingArguments, Trainer, AutoModelForQuestionAnswering, default_data_collator
 
 try:
     tokenized_datasets = load_from_disk(TOKENIZED_PATH)
-    train_dataset = SquadDataset(tokenized_datasets["train"])
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8)
     #['input_ids', 'token_type_ids', 'attention_mask', 'start_positions', 'end_positions']
-    # Load the dataset
-    #squad = load_dataset("squad")
 
-    # Load the tokenizer and model
-    #tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    #train_tokens = tokenizer(squad["train"]["question"], padding = True, truncation=True)
     print("Tokenized dataset found. Proceeding with training...")
 except FileNotFoundError:
     print(
@@ -47,41 +17,53 @@ except FileNotFoundError:
     )
     tokenized_datasets = get_tokenized_datasets()
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModelForQuestionAnswering.from_pretrained("bert-base-uncased")
 
-data_collator = DefaultDataCollator()
+#SQUAD train dataset has 87599 rows
+print("Loading half of the train dataset... It may take a while")
+train_dataloader = DataLoader(tokenized_datasets["train"], batch_size=128, collate_fn=default_data_collator)
+#For freeing GPU memory
+del tokenized_datasets
 
+"""
 args = TrainingArguments(
     "finetune-BERT-squad",
-    eval_strategy="epoch",
+    #eval_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=125,
+    per_device_train_batch_size=64,
+    #per_device_eval_batch_size=8,
+    #num_train_epochs=125,
     weight_decay=0.01,
 )
+"""
+
 trainer = Trainer(
     model=model,
-    args=args,
-    train_dataset=tokenized_datasets["train"].select(range(1000)),
-    eval_dataset=tokenized_datasets["validation"].select(range(100)),
-    data_collator=data_collator,
-    tokenizer=tokenizer,
+    #args=args,
+    #train_dataset=tokenized_datasets["train"].select(range(1000)),
+    #eval_dataset=tokenized_datasets["validation"].select(range(100)),
+    #data_collator=data_collator,
+    #tokenizer=tokenizer,
 )
-
 
 prof = torch.profiler.profile(
         schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
-        on_trace_ready=torch.profiler.tensorboard_trace_handler('./runs/profiling'),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./runs/BERTSQUAD'),
         record_shapes=True,
         with_stack=True)
 
 prof.start()
-for step, batch_data in enumerate(train_loader):
+start_time = time.time()
+#171 steps
+for step, batch_data in enumerate(train_dataloader):
         prof.step()  # Need to call this at each step to notify profiler of steps' boundary.
         if step >= 1 + 1 + 3:
             break
         trainer.training_step(model, batch_data)
+        #For freeing GPU memory
+        torch.cuda.empty_cache()
+end_time = time.time()
+execution_time = (end_time - start_time)/60
+print(f"Execution time: {execution_time:.4f} minutes")
 prof.stop()
 
